d6691254d0defc902c42f98052d2edc42a49b811
Danelly Joseph <danelly_joseph@sfu.ca>
2025-10-01 02:33:51 -0700

    changes made

--- main.js
+++ main.js
@@ -1,7 +1,59 @@
-// Simple interactive script for the website
-document.addEventListener('DOMContentLoaded', function() {
-    const header = document.querySelector('header');
-    header.addEventListener('click', function() {
-        alert('Hello, welcome to my personal website!');
+
+
+
+// main.js (Simple Star Twinkle Animation using help from SVG.js)
+
+// set canvas size
+const width = window.innerWidth;
+const height = window.innerHeight - 10;
+
+// attach to the creativeSVG container instead of body
+const canvas = SVG()
+  .addTo('#creativeSVG')
+  .size(width, height);
+
+// interval for new stars
+setInterval(() => {
+  if (Math.random() < 0.7) {
+    setTimeout(() => {
+      createStar();
+    }, Math.random() * 1000);
+  }
+}, 100);
+
+// create star
+const createStar = () => {
+  const x = Math.random() * width;
+  const y = Math.random() * height;
+
+  const size = Math.random() * 12;
+  canvas.circle(size)
+    .fill('aquamarine')
+    .center(x, y)
+    .opacity(0)
+    .animate(2000)
+    .opacity(1)
+    .delay(300)
+    .after(function () {
+      twinkle(x, y, size);
+      this.element().remove();
     });
-});
+};
+
+// The twinkle effect
+const twinkle = (x, y, size) => {
+  canvas.polygon([0, 0, 10, 2, 12, 12, 14, 2, 24, 0, 14, -2, 12, -12, 10, -2])
+    .fill('#f3eccf')
+    .center(x, y)
+    .scale(size / 10, x, y)
+    .animate(1000)
+    .scale(size / 3, x, y)
+    .opacity(0)
+    .after(function () {
+      this.element().remove();
+    });
+};
+
+
+
+
